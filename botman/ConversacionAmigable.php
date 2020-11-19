c54fe44e7c812834ffee595408362f53757929cd
wtfranco22 <francofranco_96@hotmail.com>
2020-11-19 02:21:25 -0300

    .

--- botman/ConversacionAmigable.php
+++ botman/ConversacionAmigable.php
@@ -5,21 +5,19 @@ use BotMan\BotMan\Messages\Conversations\Conversation;
 class ConversacionAmigable extends Conversation
 {
     protected $respuesta;
-
     public function charla()
     {
         $this->ask('Como estas humano?', function ($answer) {
             $respuesta = $answer->getText();
-            if (preg_match("/vos/", $respuesta)) {
+            if (preg_match("/vos?/", $respuesta)) {
                 $this->say('Bien, gracias por preguntarme');
-            } elseif(trim($respuesta) == "." || trim($respuesta) == " "){
+            } elseif(preg_match("/\W/", $respuesta)){
                 $this->say('Me estas provocando? ¬_¬ dije <br>');
                 return $this->repeat();
             }
             $this->say('que desea realizar?');
         });
     }
-
     public function run()
     {
         $this->charla();
